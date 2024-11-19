d3bab0b0d38512d482382c05d283d9a6ebf0d88b
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2024-11-19 21:15:38 +0100

    .

--- errors.go
+++ errors.go
@@ -5,5 +5,4 @@ import "errors"
 var (
 	ErrInvalidProtocol = errors.New("given protocol in rotating_proxy is not supported")
 	ErrBadIPRep        = errors.New("bad ip reputation")
-	ErrTimeout         = errors.New("timeout")
 )
